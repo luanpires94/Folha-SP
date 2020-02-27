03a41d76cf47c434942823f83bb30e0e1ddce39d
luanpires94 <luan.94pires@gmail.com>
2020-02-27 18:10:18 -0300

    ajustes no menu

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -30,23 +28,9 @@ function showSlides(n) {
 /*FIM DO SLIDE*/
 
 /*MENU*/
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+  var element = document.getElementById("myDropdown");
+  element.classList.add("show");
+}
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
   /*FIM DO MENU*/
